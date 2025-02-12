b4a6de34739a6ab477d8f23c255f6d063b939284
Brendan Graetz <bguiz@users.noreply.github.com>
2025-02-12 18:45:16 +0800

    feat: implement skeletal layouts for code_1 and code_and_console
    - NOTE no theme/ styling included yet
    
    - NOTE that these layouts are TBD: blank_canvas module_title_start overview_1 quiz_mcq_2 quiz_mcq_4 quiz_mcq_5 quiz_section_start section_title_end section_title_start statement_2 statement_icon text_chunks_2 text_chunks_3 text_chunks_4 toc
    
    Signed-off-by: Brendan Graetz <bguiz@users.noreply.github.com>

---
+++ hdc-slides/layouts/code_1.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="slidev-layout two-cols">
+      <div>
+        <slot />
+      </div>
+      <div class="tab">
+        <slot name="tab" />
+      </div>
+      <div class="code">
+        <slot name="code" />
+      </div>
+    </div>
+</template>
---
+++ hdc-slides/layouts/code_and_console.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="slidev-layout two-cols">
+      <div>
+        <slot />
+      </div>
+      <div class="tab">
+        <slot name="tab" />
+      </div>
+      <div class="code">
+        <slot name="code" />
+      </div>
+      <div class="console">
+        <slot name="console" />
+      </div>
+    </div>
+</template>
